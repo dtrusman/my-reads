4db8996a277f5178ace7f9db2067a4a8828ec3b0
Daniel Trusman <daniel.trusman@kaiostech.com>
2019-05-17 17:11:20 -0400

    Loader in search page

--- src/components/SearchItem/SearchItem.tsx
+++ src/components/SearchItem/SearchItem.tsx
@@ -1,34 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as API from '../../services/BooksAPI';
-import { Card, Changer } from '..';
+import { Card, Changer, Loader } from '..';
 
 interface Props {
     item: any;
 }
 
-const SearchItem = (props: Props) => {
-
-    const { item } = props;
-
-    const handleChangeShelf = async evt => {
-        const { item } = props;
-        const shelfID = evt.target.value;
-        await API.update(item, shelfID);
-    }
-
-    return (
-        <Card>
-            <div className="card-title">{item.title}</div>
-            <div className="image-container">
-                <img className="image" src={item.imageLinks.thumbnail} alt={item.title} />
-            </div>
-            <div className="changer-container">
-                <div className="add-book-container">
-                    <Changer enableNone={false} onChooseOption={handleChangeShelf} />
-                </div>
-            </div>
-        </Card>
-    )
+interface State {
+    loading: boolean;
 }
 
-export default SearchItem;
+export default class SearchItem extends Component<Props, State> {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false
+        }
+    }
+
+    handleChangeShelf = async evt => {
+        this.setState({ loading: true });
+        const { item } = this.props;
+        const shelfID = evt.target.value;
+        await API.update(item, shelfID);
+        this.setState({ loading: false });
+    }
+
+    renderBook = () => {
+        const { item } = this.props;
+
+        return (
+            <Card>
+                <div className="card-title">{item.title}</div>
+                <div className="image-container">
+                    <img className="image" src={item.imageLinks.thumbnail} alt={item.title} />
+                </div>
+                <div className="changer-container">
+                    <div className="add-book-container">
+                        <Changer enableNone={false} onChooseOption={this.handleChangeShelf} />
+                    </div>
+                </div>
+            </Card>
+        )
+    }
+
+    render() {
+        const { loading } = this.state;
+
+        return loading ? <Card><Loader text="Adding book..." /></Card> : this.renderBook()
+    }
+}
